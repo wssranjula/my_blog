600cb7770668cf12e062b2d5c67fd4363cf5bb76
Suresh Ranjula <50176787+wssranjula@users.noreply.github.com>
2024-01-13 16:02:31 +0530

    updated about and projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,37 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
+    title: 'A  Generative AI Portal',
+    description: `A Genrative Application Portal with UI to Code, Code Reviewer, Test Case Genrater and Enterprise GPT4V tools`,
+    imgSrc: '/static/images/aibud.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    title: 'Resume Builder',
+    description: `An Application that will Customize and Modify uploaded Resume for given new Job description
+    users will be able to add there details and generate new Resume with selected Template. 
+    Used Python ,Langchain, OpenAI , Flask 
+     `,
+    imgSrc: '/static/images/resume.png',
     href: '/blog/the-time-machine',
   },
+  {
+    title: 'Whatsapp/Teams/Skype Assistant chatbot',
+    description: `Whatsapp Assistant chatbot that will answer to your Questions about your Buisness or Applications,
+    It can take orders behlaf of you schedule meetings and much more
+    Used Python , OpenAI Assistants, AWS Lambda`,
+    imgSrc: '/static/images/wap.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Flowise/Voiceflow/BotPress Automations with Zapier/Make',
+    description: `AI Assistant chatbot that will answer to your Questions about your Buisness or Applications,
+    It can take orders behlaf of you schedule meetings and much more
+    Used Python , OpenAI Assistants, AWS Lambda`,
+    imgSrc: '/static/images/flow.png',
+    href: 'https://flowiseai.com',
+  },
+
 ]
 
 export default projectsData
